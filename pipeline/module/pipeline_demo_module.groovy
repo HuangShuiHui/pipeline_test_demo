cca99d37dca6153e8bdb1533c9dd0776f8171f16
shuihui_huang <shuihui_huang@163.com>
2019-04-17 22:37:04 +0800

    测试代码：读取文件

---
+++ pipeline/module/pipeline_demo_module.groovy
@@ -0,0 +1,8 @@
+import hudson.model.*;
+
+def find_files(filetype){
+    def files = findFiles(glob:filetype)
+    for (file in files){
+        println file.name
+    }
+}
---
+++ pipeline/src/pipeline_script_job.groovy
@@ -0,0 +1,28 @@
+import hudson.model.*;
+
+println env.JOB_NAME
+println env.BUILD_NUMBER
+println env.WORKSPACE
+
+pipeline{
+
+    agent any
+    stages{
+        stage("init") {
+            steps{
+                script{
+                    println "欢迎来到Anthony的Pipeline教程。"
+                    model_test = load env.WORKSPACE + "/pipeline/module/pipeline-demo-module.groovy"
+                }
+            }
+        }
+        stage("Test Method") {
+            steps{
+                script{
+                    log_files = model_test.find_files('**/*.log')
+                }
+            }
+        }
+    }
+}
+
